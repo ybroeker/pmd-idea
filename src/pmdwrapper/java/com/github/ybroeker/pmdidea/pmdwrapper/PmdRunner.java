ead688823ece949b913df254fc19958928aaf33e
Yannick da Silva Bröker <ybroeker@techfak.uni-bielefeld.de>
2020-11-21 02:50:11 +0100

    Disables the pmd cache if only a single file is scanned
    This prevents a populated cache from being overwritten with only one single
    entry. Single file scans usually don't benefit from the cache, so this has no
    disadvantages.
    
    Signed-off-by: Yannick da Silva Bröker <ybroeker@techfak.uni-bielefeld.de>

--- src/pmdwrapper/java/com/github/ybroeker/pmdidea/pmdwrapper/PmdRunner.java
+++ src/pmdwrapper/java/com/github/ybroeker/pmdidea/pmdwrapper/PmdRunner.java
@@ -8,14 +8,17 @@ import net.sourceforge.pmd.lang.LanguageRegistry;
 import net.sourceforge.pmd.lang.LanguageVersion;
 import net.sourceforge.pmd.util.datasource.DataSource;
 import net.sourceforge.pmd.util.datasource.FileDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static java.util.Collections.singletonList;
 
 public class PmdRunner implements Runnable {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PmdRunner.class);
 
     private final Project project;
     private final List<DataSource> files;
@@ -45,7 +48,14 @@ public class PmdRunner implements Runnable {
 
         //TODO: classpath
 
-        CacheFiles.getCacheFile(project).ifPresent(file -> pmdConfig.setAnalysisCacheLocation(file.getAbsolutePath()));
+        Optional<File> cache = CacheFiles.getCacheFile(project);
+        if (shouldUsePmdCache() && cache.isPresent()) {
+            final String cacheFile = cache.get().getAbsolutePath();
+            LOGGER.debug("PMD-Cache is used: {}", cacheFile);
+            pmdConfig.setAnalysisCacheLocation(cacheFile);
+        } else {
+            LOGGER.debug("PMD-Cache is not used: shouldUseCache: {}, cacheFile: {}", shouldUsePmdCache(), cache);
+        }
 
         final Language lang = LanguageRegistry.getLanguage("Java");
         final String type = pmdOptions.getTargetJdk();
@@ -59,6 +69,19 @@ public class PmdRunner implements Runnable {
         pmdConfig.setRuleSets(ruleSets);
 
         return pmdConfig;
+    }
+
+    /**
+     * Checks if a scan should use the cache.
+     * <p>
+     * The cache should currently only be used when scanning more than one file.
+     * This prevents individual file scans from overwriting a populated cache,
+     * which would prevent the next full project scan from benefiting from it.
+     *
+     * @return if the scan should be used
+     */
+    private boolean shouldUsePmdCache() {
+        return files.size() > 1;
     }
 
     @Override
