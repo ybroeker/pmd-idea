7b688440c4e55622ae3dae84eee20a1345e0674a
Yannick da Silva Bröker <ybroeker@techfak.uni-bielefeld.de>
2020-11-24 19:54:45 +0100

    Fix #5, read PsiFiles in read-action

--- src/main/java/com/github/ybroeker/pmdidea/inspection/ScannablePsiFile.java
+++ src/main/java/com/github/ybroeker/pmdidea/inspection/ScannablePsiFile.java
@@ -4,25 +4,27 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 
 import com.github.ybroeker.pmdidea.pmd.ScannableFile;
+import com.intellij.openapi.application.ReadAction;
 import com.intellij.psi.PsiFile;
 
 
-public class VirtuallyFile implements ScannableFile {
+public class ScannablePsiFile implements ScannableFile {
 
     private final PsiFile psiFile;
 
-    public VirtuallyFile(final PsiFile psiFile) {
+    public ScannablePsiFile(final PsiFile psiFile) {
         this.psiFile = psiFile;
     }
 
     @Override
     public String getDisplayName() {
-        return psiFile.getName();
+        return psiFile.getVirtualFile().getPresentableUrl();
     }
 
     @Override
     public InputStream getInputStream() {
-        final byte[] bytes = psiFile.getText().getBytes(StandardCharsets.UTF_8);
+        final String text = ReadAction.compute(psiFile::getText);
+        final byte[] bytes = text.getBytes(StandardCharsets.UTF_8);
         return new ByteArrayInputStream(bytes);
     }
 
